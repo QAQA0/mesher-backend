ec1dc3a5909144426ce8a14ea2a4e32704957eaf
최현호 <117135776+QAQA0@users.noreply.github.com>
2024-10-24 00:06:00 +0900

    Feat ethersService
    Cron을 이용하여 값을 가져오지 않고
    이벤트를 사용하여 최신 정보 유지

--- src/providers/ethers/ethers.service.ts
+++ src/providers/ethers/ethers.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { ethers } from 'ethers';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Block, ethers } from 'ethers';
 import * as dotenv from 'dotenv';
 import { BlockEntity } from '../../modules/block-chain/entities/block.entity';
 import { TransactionReceiptEntity } from '../../modules/block-chain/entities/transactionReceipt.entity';
@@ -10,7 +10,7 @@ import { LogEntity } from '../../modules/block-chain/entities/log.entity';
 dotenv.config();
 
 @Injectable()
-export class EthersService {
+export class EthersService implements OnModuleInit {
   private readonly network: string = process.env.ETHEREUM_NETWORK;
   private readonly infuraApiKey: string = process.env.INFURA_API_KEY;
   private provider = new ethers.InfuraProvider();
@@ -19,8 +19,12 @@ export class EthersService {
     this.provider = new ethers.InfuraProvider(this.network, this.infuraApiKey);
   }
 
+  onModuleInit() {
+    this.getLatestData();
+  }
+
   /**
-   * 블록체인 데이터 Block, TransactionReceipt, Log 값 저장
+   * 블록체인 데이터 Block, TransactionReceipt, Log 값을 저장합니다.
    * @Param blockCount (조회 개수)
    */
   async getDatas(blockCount: number): Promise<void> {
@@ -43,79 +47,21 @@ export class EthersService {
         const block = await this.provider.getBlock(latestBlockNumber - i);
 
         if (block) {
-          const blockEntity = BlockEntity.builder()
-            .setHash(block.hash)
-            .setParentHash(block.parentHash)
-            .setNumber(block.number)
-            .setTimestamp(block.timestamp)
-            .setNonce(block.nonce)
-            .setDifficulty(block.difficulty)
-            .setGasLimit(block.gasLimit)
-            .setGasUsed(block.gasUsed)
-            .setMiner(block.miner)
-            .setBaseFeePerGas(block.baseFeePerGas)
-            .setTransactions(block.transactions as string[])
-            .build();
-
-          blockEntities.push(blockEntity);
-
-          // 병렬 처리
-          const transactionPromise = block.transactions.map(async (txHash) => {
-            const receipt = await this.provider.getTransactionReceipt(txHash);
-
-            const transactionEntity = TransactionReceiptEntity.builder()
-              .setBlockHash(receipt.blockHash)
-              .setBlockNumber(receipt.blockNumber)
-              .setCumulativeGasUsed(receipt.cumulativeGasUsed)
-              .setGasPrice(receipt.gasPrice)
-              .setFrom(receipt.from)
-              .setGasUsed(receipt.gasUsed)
-              .setStatus(receipt.status)
-              .setTo(receipt.to)
-              .setTransactionHash(receipt.hash)
-              .setTransactionIndex(receipt.index)
-              .build();
-
-            transactionEntities.push(transactionEntity);
-
-            const logPromise = receipt.logs.map((transactionReceiptLog) => {
-              const logEntity = LogEntity.builder()
-                .setLogIndex(transactionReceiptLog.index)
-                .setTransactionHash(transactionReceiptLog.transactionHash)
-                .setBlockHash(transactionReceiptLog.blockHash)
-                .setBlockNumber(transactionReceiptLog.blockNumber)
-                .setAddress(transactionReceiptLog.address)
-                .setTopics(transactionReceiptLog.topics as string[])
-                .setData(transactionReceiptLog.data)
-                .build();
-
-              logEntities.push(logEntity);
-            });
-
-            await Promise.all(logPromise);
-          });
-
-          await Promise.all(transactionPromise);
+          await this.processBlock(
+            block,
+            blockEntities,
+            transactionEntities,
+            logEntities,
+          );
         }
       }
 
-      // 블록 저장
-      if (blockEntities.length > 0) {
-        await queryRunner.manager.save(BlockEntity, blockEntities);
-      }
-
-      // 트랜잭션 저장
-      if (transactionEntities.length > 0) {
-        await queryRunner.manager.save(
-          TransactionReceiptEntity,
-          transactionEntities,
-        );
-      }
-
-      // 로그 저장
-      if (logEntities.length > 0) {
-        await queryRunner.manager.save(LogEntity, logEntities);
-      }
+      await this.saveAllEntities(
+        queryRunner,
+        blockEntities,
+        transactionEntities,
+        logEntities,
+      );
 
       // 작업 완료 시 커밋
       await queryRunner.commitTransaction();
@@ -129,11 +75,147 @@ export class EthersService {
   }
 
   /**
-   * 12초마다 가장 최근에 생성된 데이터를 저장합니다.
+   * 새로 생성되는 데이터를 저장합니다.
+   * @param block
    */
-  @Cron('*/12 * * * * *')
-  async getLatestData() {
-    await this.getDatas(1);
+  async saveLatestData(block: Block): Promise<void> {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    // 트랜잭션 시작
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const blockEntities = [];
+      const transactionEntities = [];
+      const logEntities = [];
+
+      if (block) {
+        await this.processBlock(
+          block,
+          blockEntities,
+          transactionEntities,
+          logEntities,
+        );
+      }
+
+      await this.saveAllEntities(
+        queryRunner,
+        blockEntities,
+        transactionEntities,
+        logEntities,
+      );
+
+      // 작업 완료 시 커밋
+      await queryRunner.commitTransaction();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      // 트랜잭션 종료
+      await queryRunner.release();
+    }
+  }
+
+  // 블록, 트랜잭션, 로그 엔티티 생성
+  private async processBlock(
+    block: Block,
+    blockEntities: BlockEntity[],
+    transactionEntities: TransactionReceiptEntity[],
+    logEntities: LogEntity[],
+  ): Promise<void> {
+    const blockEntity = BlockEntity.builder()
+      .setHash(block.hash)
+      .setParentHash(block.parentHash)
+      .setNumber(block.number)
+      .setTimestamp(block.timestamp)
+      .setNonce(block.nonce)
+      .setDifficulty(block.difficulty)
+      .setGasLimit(block.gasLimit)
+      .setGasUsed(block.gasUsed)
+      .setMiner(block.miner)
+      .setBaseFeePerGas(block.baseFeePerGas)
+      .setTransactions(block.transactions as string[])
+      .build();
+
+    blockEntities.push(blockEntity);
+
+    // 병렬 처리
+    const transactionPromise = block.transactions.map(async (txHash) => {
+      const receipt = await this.provider.getTransactionReceipt(txHash);
+
+      const transactionEntity = TransactionReceiptEntity.builder()
+        .setBlockHash(receipt.blockHash)
+        .setBlockNumber(receipt.blockNumber)
+        .setCumulativeGasUsed(receipt.cumulativeGasUsed)
+        .setGasPrice(receipt.gasPrice)
+        .setFrom(receipt.from)
+        .setGasUsed(receipt.gasUsed)
+        .setStatus(receipt.status)
+        .setTo(receipt.to)
+        .setTransactionHash(receipt.hash)
+        .setTransactionIndex(receipt.index)
+        .build();
+
+      transactionEntities.push(transactionEntity);
+
+      const logPromise = receipt.logs.map((transactionReceiptLog) => {
+        const logEntity = LogEntity.builder()
+          .setLogIndex(transactionReceiptLog.index)
+          .setTransactionHash(transactionReceiptLog.transactionHash)
+          .setBlockHash(transactionReceiptLog.blockHash)
+          .setBlockNumber(transactionReceiptLog.blockNumber)
+          .setAddress(transactionReceiptLog.address)
+          .setTopics(transactionReceiptLog.topics as string[])
+          .setData(transactionReceiptLog.data)
+          .build();
+
+        logEntities.push(logEntity);
+      });
+
+      await Promise.all(logPromise);
+    });
+
+    await Promise.all(transactionPromise);
+  }
+
+  // 엔티티 저장
+  private async saveAllEntities(
+    queryRunner: any,
+    blockEntities: BlockEntity[],
+    transactionEntities: TransactionReceiptEntity[],
+    logEntities: LogEntity[],
+  ): Promise<void> {
+    if (blockEntities.length > 0) {
+      await queryRunner.manager.save(BlockEntity, blockEntities);
+    }
+
+    if (transactionEntities.length > 0) {
+      await queryRunner.manager.save(
+        TransactionReceiptEntity,
+        transactionEntities,
+      );
+    }
+
+    if (logEntities.length > 0) {
+      await queryRunner.manager.save(LogEntity, logEntities);
+    }
+  }
+
+  /**
+   * 가장 최근에 생성된 데이터를 가져옵니다.
+   */
+  getLatestData() {
+    this.provider.on('block', async (blockNumber: number) => {
+      try {
+        const block = await this.provider.getBlock(blockNumber);
+        if (block) {
+          await this.saveLatestData(block);
+        }
+      } catch (error) {
+        console.error('Error processing block:', error);
+      }
+    });
   }
 
   /**
